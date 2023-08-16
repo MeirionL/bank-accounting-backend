f2eb84e15cb7a0fc145981d62fa817adeb725473
Meirion <meirionlayton@gmail.com>
2023-08-16 12:01:53 +0100

    first draft http boilerplate complete

--- main.go
+++ main.go
@@ -2,25 +2,51 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
+var Total float64 = 885.03
+var CurrId int = 3
+
 type transaction struct {
-	ID        string  `json:"id"`
-	Payment   float64 `json:"payment"`
-	PreTotal  float64 `json:"pretotal"`
-	Recipient string  `json:"recipient"`
+	ID            string  `json:"id"`
+	Payment       float64 `json:"payment"`
+	PreTotal      float64 `json:"preTotal"`
+	PostTotal     float64 `json:"postTotal"`
+	Recipient     string  `json:"recipient"`
+	AccountNumber string  `json:"accountNumber"`
+	SortCode      string  `json:"sortCode"`
+	PaidBefore    bool    `json:"paidBefore"`
+	Time          string  `json:"time"`
+	Date          string  `json:"data"`
+	Message       string  `json:"message"`
 }
 
 var transactions = []transaction{
-	{ID: "1", Payment: 56.99, PreTotal: 200, Recipient: "John"},
-	{ID: "2", Payment: 17.99, PreTotal: 300, Recipient: "Connor"},
-	{ID: "3", Payment: 39.99, PreTotal: 132.47, Recipient: "Doyle"},
+	{ID: "1", Payment: 56.99, PreTotal: 1000, PostTotal: 943.01, Recipient: "John", AccountNumber: "95028403", SortCode: "23-87-04", Time: time.Now().Format("15:04"), Date: time.Now().Format("2006-01-02")},
+	{ID: "2", Payment: 17.99, PreTotal: 943.01, PostTotal: 925.02, Recipient: "Connor", AccountNumber: "59058024", SortCode: "67-98-23", Time: time.Now().Format("15:04"), Date: time.Now().Format("2006-01-02"), Message: "Yes mate"},
+	{ID: "3", Payment: 39.99, PreTotal: 925.02, PostTotal: 885.03, Recipient: "Doyle", AccountNumber: "58301109", SortCode: "56-17-56", Time: time.Now().Format("15:04"), Date: time.Now().Format("2006-01-02"), Message: "Payment for XYZ"},
+}
+
+func getBalance(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, gin.H{"balance": Total})
+}
+
+func getLastTransaction(c *gin.Context) {
+	transaction, err := getTransactionById(fmt.Sprint(CurrId))
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "transaction not found"})
+	}
+
+	c.IndentedJSON(http.StatusOK, transaction)
 }
 
 func getTransactions(c *gin.Context) {
@@ -28,26 +54,38 @@ func getTransactions(c *gin.Context) {
 }
 
 func postTransactions(c *gin.Context) {
-	// NEEDS LOGIC FOR FINDING APPROPRIATE PARAMETERS AND SUBTRACTING THEM
-	id, ok := c.GetQuery("id")
-	if ok == false {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing transaction id"})
-		return
-	}
-
-	transaction, err := getTransactionById(id)
-
-	if err != nil {
-		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Transaction not found"})
-		return
-	}
-
 	var newTransaction transaction
 	if err := c.BindJSON(&newTransaction); err != nil {
 		return
 	}
+
+	CurrId++
+	newTransaction.ID = strconv.Itoa(CurrId)
+
+	newTransaction.PreTotal = Total
+	Total -= newTransaction.Payment
+	newTransaction.PostTotal = Total
+
+	if hasPrevTransaction(newTransaction.AccountNumber, newTransaction.SortCode) {
+		newTransaction.PaidBefore = true
+	} else {
+		newTransaction.PaidBefore = false
+	}
+
+	newTransaction.Time = fmt.Sprint(time.Now().Format("15:04"))
+	newTransaction.Date = fmt.Sprint(time.Now().Format("2006-01-02"))
+
 	transactions = append(transactions, newTransaction)
 	c.IndentedJSON(http.StatusCreated, newTransaction)
+}
+
+func hasPrevTransaction(an, sc string) bool {
+	for _, t := range transactions {
+		if t.AccountNumber == an && t.SortCode == sc {
+			return true
+		}
+	}
+	return false
 }
 
 func transactionById(c *gin.Context) {
@@ -63,8 +101,8 @@ func transactionById(c *gin.Context) {
 }
 
 func getTransactionById(id string) (*transaction, error) {
-	for i, b := range transactions {
-		if b.ID == id {
+	for i, t := range transactions {
+		if t.ID == id {
 			return &transactions[i], nil
 		}
 	}
@@ -80,7 +118,9 @@ func main() {
 		log.Fatal("port is not found in the environment")
 	}
 
-	router := gin.Default()                      //initialising router
+	router := gin.Default() //initialising router
+	router.GET("/balance", getBalance)
+	router.GET("/lastTransaction", getLastTransaction)
 	router.GET("/transactions", getTransactions) // sending a GET request to /transactions calls the getTransactions function
 	router.GET("/transactions/:id", transactionById)
 	router.POST("/transactions", postTransactions)
